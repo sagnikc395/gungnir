23daebe34babf08b3edfe826cc2a4bee802dfe37
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-11-06 15:21:10 +0530

    src: added parse method

--- smollisp.py
+++ smollisp.py
@@ -1,8 +1,9 @@
-from smollisp.parsing import tokenize 
+from smollisp.parsing import tokenize,parse
 
 def main():
     program = "(begin (define r 10) (* pi (* r r)))"
     print(tokenize(program))
+    print(parse(program))
 
 if __name__ == '__main__':
     main()

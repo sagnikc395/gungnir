e22f01007ce17a472bc788e9626c60691803d7f0
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-11-06 15:40:56 +0530

    src: added eval

--- smollisp.py
+++ smollisp.py
@@ -1,9 +1,11 @@
 from smollisp.parsing import tokenize,parse
+from smollisp.eval import eval 
 
 def main():
     program = "(begin (define r 10) (* pi (* r r)))"
     print(tokenize(program))
     print(parse(program))
+    print(eval(parse(program)))
 
 if __name__ == '__main__':
     main()

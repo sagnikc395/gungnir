06993ff2b5849a9465aa4bc7fdf6a84f91bef2fd
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-11-06 15:20:13 +0530

    src: added methods for parse, read_from_tokens and atom

--- smollisp/parsing.py
+++ smollisp/parsing.py
@@ -1,6 +1,44 @@
+from .types import Exp, Atom, Symbol
+
 def tokenize(chars: str) -> list:
     '''
     convert a string of chars into a list of tokens
     '''
     return chars.replace('(',' ( ').replace(')',' ) ').split()
 
+def parse(program: str) -> Exp: # type: ignore
+    '''
+    read a lisp expression from a string 
+    '''
+    return read_from_tokens(tokenize(program))
+
+def read_from_tokens(tokens: list) -> Exp: # type: ignore
+    '''
+    read an expression from a sequence of tokens 
+    '''
+    if len(tokens) == 0 :
+        raise SyntaxError("unexpected EOF")
+    token = tokens.pop(0)
+    if token == '(':
+        L = []
+        while tokens[0] !=')':
+            L.append(read_from_tokens(tokens))
+        tokens.pop(0)
+        return L 
+    elif token ==')':
+        raise SyntaxError("unexpected )")
+    else:
+        return atom(token)
+    
+def atom(token: str) -> Atom: # type: ignore
+    '''
+    numbers are numbers, every other token is a symbol
+    '''
+    try:
+        return int(token)
+    except ValueError:
+        try:
+            return float(token)
+        except ValueError:
+            return Symbol(token)
+        

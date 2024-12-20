276901ac87a1e035dca2e94d54efc37a379d700c
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-20 17:32:22 +0530

    src: added read from tokens, atom methods

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { List } from "./types";
+import type { Atom, Exp, List } from "./types";
 
 function tokenize(chars: string): List {
   //convert a string of chars into a list of tokens
@@ -9,9 +9,45 @@ function tokenize(chars: string): List {
     .filter((token) => token.length > 0);
 }
 
-const main = () => {
-  const program = "(begin (define r 10) (* pi (* r r)))";
-  console.log(tokenize(program));
-};
+function parse(program: string): Exp {
+  //read a scheme experssion from a string
+  return readFromTokens(tokenize(program));
+}
 
-main();
+function readFromTokens(tokens: List): Exp {
+  if (tokens.length === 0) {
+    throw new SyntaxError("Unexpected EOF");
+  }
+  let token = tokens.shift();
+  if (token === "(") {
+    const list: List = [];
+    while (tokens[0] !== ")") {
+      list.push(readFromTokens(tokens) as string);
+    }
+    tokens.shift();
+    return list;
+  } else if (token === ")") {
+    throw new SyntaxError("unexpected )");
+  } else {
+    return atom(token!);
+  }
+}
+
+function atom(token: string): Atom {
+  // numbers become numbers, other token is a symbol
+  const intval = parseInt(token);
+  if (!isNaN(intval) && intval.toString() === token) {
+    return intval;
+  }
+
+  //parsing as float
+  const floatval = parseFloat(token);
+  if (!isNaN(floatval) && floatval.toString() === token) {
+    return floatval;
+  }
+  //return as symbol
+  return token;
+}
+
+const program = "(begin (define r 10) (* pi (* r r)))";
+console.log(tokenize(program));
--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,6 @@
 export type Symbol = string;
 export type Number = number;
 export type Atom = Symbol | Number;
-export type List = typeof Array;
+export type List = Array<string>;
 export type Exp = Atom | List;
 export type Env = typeof Map;

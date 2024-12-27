bda0db20ccb3703a5cc3a4bcd3815c05bdcb9949
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-27 23:48:05 +0530

    step0 passes

---
+++ gungnir/gungnir.py
@@ -0,0 +1,14 @@
+def read(input):
+    return input
+
+def eval_input(input):
+    return input
+
+def rep(input):
+    return input
+
+def main():
+    while True:
+        prompt = input("user> ")
+        res = rep(prompt)
+        print(res)
---
+++ main.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    from gungnir import gungnir
+    gungnir.main()

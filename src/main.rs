f17cb312ac585bf2fb96acc41465da134fce3706
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-02-01 09:54:59 +0530

    src: added tokenize method

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,17 @@ fn parse(program: &mut str) -> Vec<&str> {
     todo!()
 }
 
+fn tokenize(chars: &str) -> Vec<String> {
+    //convert a string of characters into a list of tokens
+
+    chars
+        .replace("(", " ( ")
+        .replace(")", " ) ")
+        .split_whitespace()
+        .map(String::from)
+        .collect()
+}
+
 fn main() {
     let program = "(begin (define r 10) (* pi (* r r)))";
 }

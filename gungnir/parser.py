a7a7e7b35d758ac25470bf242edd9858b491bb33
sagnikc395 <sagnikchatterjee607@gmail.com>
2025-07-04 20:33:26 +0530

    feat: read from tokens, parse implemented

--- gungnir/parser.py
+++ gungnir/parser.py
@@ -1,8 +1,16 @@
 # using direct string split and doing on that
 
+from .types import Expression
+
 class Parser:
-    @staticmethod
-    def tokenize(chars: str) -> list:
+    def tokenize(self,chars: str) -> list:
         return chars.replace('(',' ( ').replace(')',' ) ').split()
 
+    def parse(self,program: str) -> Expression:
+        '''
+            read a expression from a string 
+        '''
+        return self.read_from_tokens(self.tokenize(program))
 
+    def read_from_tokens(tokens: list) -> Expression:
+        pass  
